0bbe34a03b58415305d03d873feae7d4669358c2
alvin zhao <yibozhao2018@gmail.com>
2021-01-04 10:39:49 -0800

    03

--- advanced_methods_exercise/03_only_vowels.rb
+++ advanced_methods_exercise/03_only_vowels.rb
@@ -2,6 +2,24 @@
 # The method should return true if the string contains only vowels.
 # The method should return false otherwise.
 
+# def only_vowels?(str)
+#     str.each_char do |char|
+#         if !'aeiou'.include?(char.downcase)
+#             return false
+#         end
+#     end
+#     true
+# end
+
+# include?
+
+def only_vowels?(str)
+    str.split("").all? do |c|
+        "aeiou".include?(c.downcase)
+    end
+end
+# all? any? none? is for array 
+#  split(""), join("")
 
 p only_vowels?("aaoeee")  # => true
 p only_vowels?("iou")     # => true
