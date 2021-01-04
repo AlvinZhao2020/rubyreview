b5884a21f9b37839c3bd0fa9c4e4df30312dd01a
alvin zhao <yibozhao2018@gmail.com>
2021-01-04 10:28:34 -0800

    01

--- advanced_methods_exercise/01_coprime.rb
+++ advanced_methods_exercise/01_coprime.rb
@@ -3,8 +3,20 @@
 # The method should return false otherwise. For example coprime?(25, 12) is true because
 # 1 is the only number that divides both 25 and 12.
 
+def coprime?(n1,n2)
+    (2..n1).each do |el|
+        if n1%el == 0 && n2%el == 0 
+            return false
+        end
+    end
+    return true
+end
+
+
 
 p coprime?(25, 12)    # => true
 p coprime?(7, 11)     # => true
 p coprime?(30, 9)     # => false
 p coprime?(6, 24)     # => false
+
+
--- foundation/jan4.rb
+++ foundation/jan4.rb
@@ -7,3 +7,15 @@ p arr.inject{|acc,el| acc+ el}
 p arr.inject(10){|acc,el| acc+ el}
 
 # scope
+
+def say_hello
+    message = 'hi'
+    p message
+end
+
+say_hello
+
+# p message 
+
+# bubble sort 
+
